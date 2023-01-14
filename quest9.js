a2ef25c19cf19ef150c0352d5a56333621327535
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-15 01:40:38 +0530

    feat: add functionality to replace each character with their next ascii

---
+++ quest9.js
@@ -0,0 +1,12 @@
+const replaceEachCharWithNextAsciiCharArrow= (str)=> {
+    return str.split('').map(chr=> String.fromCharCode(chr.charCodeAt(0)+1)).join('');
+}
+
+function replaceEachCharWithNextAsciiCharRegular(str) {
+    return str.split('').map(chr=> String.fromCharCode(chr.charCodeAt(0)+1)).join('');
+}
+
+module.exports= {
+    replaceEachCharWithNextAsciiCharArrow,
+    replaceEachCharWithNextAsciiCharRegular
+}
