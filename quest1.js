a29ca33830b0f08b770fa04461f0f13e667cbc7b
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-15 01:52:30 +0530

    chore: gitignore node modules

--- quest1.js
+++ quest1.js
@@ -6,14 +6,14 @@ const getLargestEvenArrow= (arr)=> {
     if (arr.some(itm=> typeof itm !== "number")) return -1   
 
     // filter out even numbers
-    const evenNums= arr.filter(itm=> item%2=== 0)
+    const evenNums= arr.filter(itm=> itm%2=== 0)
 
     // if len== 0: return -1
     if (evenNums.length=== 0) return -1
 
     // return max num
     // return Math.max(...evenNums)
-    return arr.reduce((a, b)=> a>b? a: b)
+    return evenNums.reduce((a, b)=> a>b? a: b)
 }
 
 function getLargestEvenRegular(arr) {
@@ -24,7 +24,7 @@ function getLargestEvenRegular(arr) {
     if (arr.some(itm=> typeof itm !== "number")) return -1   
 
     // filter out even numbers
-    const evenNums= arr.filter(itm=> item%2=== 0)
+    const evenNums= arr.filter(itm=> itm%2=== 0)
 
     // if len== 0: return -1
     if (evenNums.length=== 0) return -1
