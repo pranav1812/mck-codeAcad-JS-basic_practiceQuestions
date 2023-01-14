f813003d0b60d7a655316f4ffd1c20951a0fc85d
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-15 01:39:53 +0530

    feat: add functionality to count number of vowels

---
+++ quest8.js
@@ -0,0 +1,16 @@
+const countVowelsArrow= (str)=> {
+    let vowels= ['a', 'e', 'i', 'o', 'u'];
+    let vowelsInStr= str.split('').filter(chr=> vowels.includes(chr));
+    return vowelsInStr.length;
+}
+
+function countVowelsRegular(str) {
+    let vowels= ['a', 'e', 'i', 'o', 'u'];
+    let vowelsInStr= str.split('').filter(chr=> vowels.includes(chr));
+    return vowelsInStr.length;
+}
+
+module.exports= {
+    countVowelsArrow,
+    countVowelsRegular
+}
