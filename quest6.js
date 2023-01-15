700760bb6a2f1a69f67f75bb23df2e32d96dbff7
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-15 18:11:01 +0530

    chore: write tests for quest6 functions

--- quest6.js
+++ quest6.js
@@ -1,9 +1,11 @@
 const endsWithScriptArrow= (str)=> {
+    if (typeof str!= "string" || str.length< 6) return -1
     let lastSix= str.slice(-6);
     return lastSix === 'Script';
 }
 
 function endsWithScriptRegular(str) {
+    if (typeof str!= "string" || str.length< 6) return -1
     let lastSix= str.slice(-6);
     return lastSix === 'Script';
 }
---
+++ tests/quest6.test.js
@@ -0,0 +1,17 @@
+const {
+    endsWithScriptArrow
+}= require('../quest6');
+
+// should only accept strings with length >= 6
+test("should only except strings with length >= 6", ()=> {
+    expect(endsWithScriptArrow(345)).toBe(-1);
+    expect(endsWithScriptArrow(null)).toBe(-1);
+    expect(endsWithScriptArrow("apple")).toBe(-1);
+})
+
+// should return whether the 6 or higher length string ends with Script
+test("should return whether the 6 or higher length string ends with 'Script'", ()=> {
+    expect(endsWithScriptArrow("mckinsey")).toBe(false);
+    expect(endsWithScriptArrow("javaScript")).toBe(true);
+    expect(endsWithScriptArrow("typescript")).toBe(false);
+})
