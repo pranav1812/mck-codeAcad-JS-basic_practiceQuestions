24cd2188cada76993018e67ccd71884c92d3be04
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-15 18:05:02 +0530

    refractor: invalid argument handling in quest4 and quest5 functions

--- quest4.js
+++ quest4.js
@@ -1,8 +1,14 @@
 const findLongestWordInArrayArrow= (arr)=> {
+    if (!Array.isArray(arr)) return -1
+    if (arr.some(val=> typeof val !== 'string' )) return -1
+    if (arr.length== 0) return -1
     return arr.reduce((x, y)=> x.length > y.length ? x : y)
 }
 
 function findLongestWordInArrayRegular(arr) {
+    if (!Array.isArray(arr)) return -1
+    if (arr.some(val=> typeof val !== 'string' )) return -1
+    if (arr.length== 0) return -1
     return arr.reduce(function(x, y) {
         return x.length > y.length ? x : y
     })
--- quest5.js
+++ quest5.js
@@ -1,8 +1,10 @@
 const isLeapYearArrow= (year)=> {
+    if (typeof year!= 'number' || year< 0 || year%1!== 0) return -1
     return year%4!== 0? false : year%100!== 0? true : year%400!== 0? false : true;
 }
 
 function isLeapYearRegular(year) {
+    if (typeof year!= 'number' || year< 0 || year%1!== 0) return -1
     return year%4!== 0? false : year%100!== 0? true : year%400!== 0? false : true;
 }
 
