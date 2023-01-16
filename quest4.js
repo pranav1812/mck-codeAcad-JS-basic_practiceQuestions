f5fc0d9b3ff0527377b329cf335193503da891e9
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-16 12:28:13 +0530

    feat: add functionality to prioritize lexicographically smallest string in findLongestString function

--- quest4.js
+++ quest4.js
@@ -2,7 +2,7 @@ const findLongestWordInArrayArrow= (arr)=> {
     if (!Array.isArray(arr)) return -1
     if (arr.some(val=> typeof val !== 'string' )) return -1
     if (arr.length== 0) return -1
-    return arr.reduce((x, y)=> x.length > y.length ? x : y)
+    return arr.reduce((x, y)=> x.length > y.length ? x : x.length< y.length? y : x< y? x: y)
 }
 
 function findLongestWordInArrayRegular(arr) {
@@ -10,7 +10,7 @@ function findLongestWordInArrayRegular(arr) {
     if (arr.some(val=> typeof val !== 'string' )) return -1
     if (arr.length== 0) return -1
     return arr.reduce(function(x, y) {
-        return x.length > y.length ? x : y
+        return x.length > y.length ? x : x.length< y.length? y : x< y? x: y
     })
 }
 
