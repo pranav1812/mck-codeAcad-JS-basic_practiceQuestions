ed4911ffea29dbca9e14650190e6a5abca698197
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-16 13:01:14 +0530

    refractor: find the lasrgest even in 1 reduce statement

--- quest1.js
+++ quest1.js
@@ -5,15 +5,17 @@ const getLargestEvenArrow= (arr)=> {
     // all elements should be of type integer
     if (arr.some(itm=> typeof itm !== "number")) return -1   
 
-    // filter out even numbers
-    const evenNums= arr.filter(itm=> itm%2=== 0)
+    if (!arr.some(itm=> itm%2 === 0)) return -1
 
-    // if len== 0: return -1
-    if (evenNums.length=== 0) return -1
+    // // filter out even numbers
+    // const evenNums= arr.filter(itm=> itm%2=== 0)
+
+    // // if len== 0: return -1
+    // if (evenNums.length=== 0) return -1
 
     // return max num
     // return Math.max(...evenNums)
-    return evenNums.reduce((a, b)=> a>b? a: b)
+    return arr.reduce((acc, curr)=> acc%2=== 0? curr%2=== 0? acc> curr? acc: curr : acc : curr)
 }
 
 function getLargestEvenRegular(arr) {
