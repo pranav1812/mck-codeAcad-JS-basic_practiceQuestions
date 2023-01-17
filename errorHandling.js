e1fea110442dc1b54b6c3a03c8dfee292fec963c
pranav_vohra <pranavvohra1812@yahoo.in>
2023-01-17 15:46:34 +0530

    fix: do not take 0 as false in divide function to check for inappropriate args

--- errorHandling.js
+++ errorHandling.js
@@ -1,7 +1,7 @@
 // let divideByZero= (num)=> num/0;
 
 let divide= (dividend, divisor)=> {
-  if (!dividend || !divisor) throw new Error('inappropriate args: (dividend, divisor)');
+  if (dividend== null || divisor== null) throw new Error('inappropriate args: (dividend, divisor)');
   if (typeof divisor!= 'number' || typeof dividend!= 'number') throw new Error('datatype not allowed');
   if (divisor=== 0) throw new Error('Operation not allowed division by zero');
   return dividend/divisor;
